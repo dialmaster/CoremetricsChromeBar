8ba7fa11cfaae6f976c03d05e02e9b6994b268e8
Chris Dial <dial.chris@gmail.com>
2013-04-26 21:19:42 -0700

    Initial commit

---
+++ gettags.js
@@ -0,0 +1,99 @@
+console.log('----------Coremetrics Tagbar for Chrome-----------');
+
+// Create the window to do the tagging in
+chrome.windows.create({
+    type: 'popup',
+    url: "tagwindow.html",
+    top: 100,
+    left: 100,
+    width: 800,
+    height: 760
+}, function (newWindow) {
+    console.log(newWindow);
+});
+
+var ourTabId;
+
+// We need to receive the tabID for the new window that this extension creates to display the tag data.
+chrome.extension.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log('Hey, the background script got a message!' +request.greeting);
+        ourTabId = request.greeting;
+    } 
+);
+
+// Listen for HTTP requests. Filter on eluminate and then send the data to our window
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    console.log('Request: ' + details.url);
+    var siteUrl = details.url.split('/')[2];
+    if (details.url.indexOf("ci=") != -1) {
+     var queryString = {};
+     
+     var theseParams = details.url.split("?")[1];
+     theseParams.split("&").forEach(function (pair) {
+       if (pair === "") return;
+       var parts = pair.split("=");
+       //queryString[parts[0]] = parts[1] && decodeURIComponent(parts[1].replace(/\+/g, " "));
+       queryString[parts[0]] = unescape(parts[1]);
+     }); 
+
+      
+      // Send a message about this tag...
+      // Fails currently -- it needs to be able to know what the id of the popup window we made is.
+      if (ourTabId) {
+        console.log("Sending a message to the tab " + ourTabId);
+          chrome.tabs.sendMessage(ourTabId, {
+            "siteUrl": siteUrl,
+            "tagType": queryString['tid'] || '',  
+            "clientId": queryString['ci'] || '',          
+            "category": queryString['cg'] || '',
+            "pageId": queryString['pi'] || '',
+            "actionType": queryString['at'] || '',
+            "referralUrl": queryString['rf'] || '',
+            "destinationUrl": queryString['ul'] || '',
+            "libraryVersion": queryString['vn1'] || '',
+            "productId": queryString['pr'] || '',
+            "productName": queryString['pm'] || '',
+            "at1": queryString['pr_a1'] || '', 
+            "at2": queryString['pr_a2'] || '', 
+            "at3": queryString['pr_a3'] || '', 
+            "at4": queryString['pr_a4'] || '', 
+            "at5": queryString['pr_a5'] || '', 
+            "at6": queryString['pr_a6'] || '',
+            "s1": queryString['s_a1'] || '', 
+            "s2": queryString['s_a2'] || '', 
+            "s3": queryString['s_a3'] || '', 
+            "s4": queryString['s_a4'] || '', 
+            "s5": queryString['s_a5'] || '', 
+            "s6": queryString['s_a6'] || '',
+            "targetUrl": queryString['hr'] || '',
+            "quantity": queryString['qt'] || '',
+            "basePrice": queryString['bp'] || '',
+            "slotNumber": queryString['sn'] || '',
+            "ps1": queryString['ps1'] || '', 
+            "ps2": queryString['ps2'] || '', 
+            "ps3": queryString['ps3'] || '', 
+            "ps4": queryString['ps4'] || '', 
+            "ps5": queryString['ps5'] || '', 
+            "ps6": queryString['ps6'] || '', 
+            "ps7": queryString['ps7'] || '', 
+            "conversionId": queryString['cid'] || '', 
+            "conversionAction": queryString['cat'] || '', 
+            "conversionCat": queryString['ccid'] || '', 
+            "conversionPoints": queryString['cpt'] || '', 
+            "lineNumber": queryString['li'] || '', 
+            "eid": queryString['eid'] || '', 
+            "ecat": queryString['ecat'] || '', 
+            "cd": queryString['cd'] || ''
+
+          }, function(res){console.log(res);});  
+      }
+    
+    }
+   
+ 
+    return {requestHeaders: details.requestHeaders};
+  },
+  {urls: ["<all_urls>"]},
+  ["blocking", "requestHeaders"]);
---
+++ tagwindow.js
@@ -0,0 +1,121 @@
+function clearHandler() {
+ $('.dialfooter').empty();
+}
+
+function markHandler() {
+$('.dialfooter').prepend('<hr style="height:5px;border-width:0;color:#AA0000;background-color:#AA0000">');
+}
+
+// Any hiding or unhiding of tag divs will happen here.
+// For now we just allow filtering on SITE or whether the "Exclude Element Tags" checkbox is checked
+
+function filterTags() {
+  var thisSite = $('.sitefilter').val();
+  var excludeElement = $('.excludeelement').is(':checked');
+
+  console.log('Filter by site ' + thisSite);
+  // Find all divs classed "datagrid" that DO NOT match the site, and hide them (unhide this site)
+  $('.datagrid').each(function() {
+    if ($(this).attr('site').indexOf(thisSite) != -1) {
+      if ( excludeElement && $(this).attr('tagtype') == '15') {
+        $(this).hide();
+      } else {
+        $(this).show();
+      }
+    } else {
+      $(this).hide();
+    }
+ 
+  });
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.clear').addEventListener('click', clearHandler);
+});
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.mark').addEventListener('click', markHandler);
+});
+document.addEventListener('DOMContentLoaded', function () { 
+  document.querySelector('.sitefilter').addEventListener('input', filterTags);
+});
+document.addEventListener('DOMContentLoaded', function () { 
+  document.querySelector('.excludeelement').addEventListener('click', filterTags);
+});
+
+
+// Get current tabid and send it as a message to the background as a 'greeting'
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+  chrome.extension.sendMessage({greeting: tabs[0].id}, function(response) {
+    console.log(response);
+  });
+} );
+
+var tid_hash = { 1: "Page View Tag",
+                     2: "Registration Tag",
+                     3: "Order Info Tag",
+                     4: "Shop Tag",
+                     5: "Product View Tag",
+                     6: "Technical Properties Tag",
+                     7: "Custom Details Tag",
+                     8: "Link Click Tag",
+                     9: "Link Impressions Tag",
+                     10: "Form Action Tag",
+                     14: "Conversion Event Tag",
+                     15: "Element Tag",
+                     404: "Error Tag" };
+
+
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    var whichSite = request.destinationUrl.split('/')[2];
+   $('.dialfooter').prepend(
+      '<div class="datagrid" site="' + whichSite + '" tagtype="' + request.tagType + '"><table>' + 
+      '<thead><tr><th colspan="2">' + tid_hash[request.tagType] + ' (tid "'+ request.tagType + '")' + ' (' + request.siteUrl + ')</th></tr></thead>' +
+      '<tbody>'+ 
+      (request.pageId ? '<tr><td width="100px">Page ID</td><td>' + request.pageId + '</td></tr>' : '') +
+      (request.targetUrl ? '<tr><td width="100px">Target HREF/URL</td><td>' + request.targetUrl + '</td></tr>' : '') +
+      (request.clientId ? '<tr><td width="100px">Client ID</td><td>' + request.clientId + '</td></tr>' : '') +
+      (request.category ? '<tr><td width="100px">Category ID</td><td>' + request.category + '</td></tr>' : '') +
+      (request.libraryVersion ? '<tr><td width="100px">Library Version</td><td>' + request.libraryVersion + '</td></tr>' : '') +
+      (request.conversionId ? '<tr><td width="100px">Conversion Event ID</td><td>' + request.conversionId + '</td></tr>' : '') +
+      (request.conversionAction ? '<tr><td width="100px">Conversion Action Type</td><td>' + request.conversionAction + '</td></tr>' : '') +
+      (request.conversionCat ? '<tr><td width="100px">Conversion Category ID</td><td>' + request.conversionCat + '</td></tr>' : '') +
+      (request.conversionPoints ? '<tr><td width="100px">Conversion Event Points</td><td>' + request.conversionPoints + '</td></tr>' : '') +
+      (request.productId ? '<tr><td width="100px">Product ID</td><td>' + request.productId + '</td></tr>' : '') +
+      (request.productName ? '<tr><td width="100px">Product Name</td><td>' + request.productName + '</td></tr>' : '') +
+      (request.quantity ? '<tr><td width="100px">Quantity</td><td>' + request.quantity + '</td></tr>' : '') +
+      (request.basePrice ? '<tr><td width="100px">Base Price</td><td>' + request.basePrice + '</td></tr>' : '') +
+      (request.slotNumber ? '<tr><td width="100px">Slot Number</td><td>' + request.slotNumber + '</td></tr>' : '') +
+      (request.actionType ? '<tr><td width="100px">Action Type</td><td>' + request.actionType + '</td></tr>' : '') +
+      (request.at1 ? '<tr><td width="100px">Attribute 1 (Explore)</td><td>' + request.at1 + '</td></tr>' : '') +
+      (request.at2 ? '<tr><td width="100px">Attribute 2 (Explore)</td><td>' + request.at2 + '</td></tr>' : '') +
+      (request.at3 ? '<tr><td width="100px">Attribute 3 (Explore)</td><td>' + request.at3 + '</td></tr>' : '') +
+      (request.at4 ? '<tr><td width="100px">Attribute 4 (Explore)</td><td>' + request.at4 + '</td></tr>' : '') +
+      (request.at5 ? '<tr><td width="100px">Attribute 5 (Explore)</td><td>' + request.at5 + '</td></tr>' : '') +
+      (request.at6 ? '<tr><td width="100px">Attribute 6 (Explore)</td><td>' + request.at6 + '</td></tr>' : '') +
+      (request.s1 ? '<tr><td width="100px">Attribute 1 (Explore)</td><td>' + request.s1 + '</td></tr>' : '') +
+      (request.s2 ? '<tr><td width="100px">Attribute 2 (Explore)</td><td>' + request.s2 + '</td></tr>' : '') +
+      (request.s3 ? '<tr><td width="100px">Attribute 3 (Explore)</td><td>' + request.s3 + '</td></tr>' : '') +
+      (request.s4 ? '<tr><td width="100px">Attribute 4 (Explore)</td><td>' + request.s4 + '</td></tr>' : '') +
+      (request.s5 ? '<tr><td width="100px">Attribute 5 (Explore)</td><td>' + request.s5 + '</td></tr>' : '') +
+      (request.s6 ? '<tr><td width="100px">Attribute 6 (Explore)</td><td>' + request.s6 + '</td></tr>' : '') +
+      (request.ps1 ? '<tr><td width="100px">Extra Field 1</td><td>' + request.ps1 + '</td></tr>' : '') +
+      (request.ps2 ? '<tr><td width="100px">Extra Field 2</td><td>' + request.ps2 + '</td></tr>' : '') +
+      (request.ps3 ? '<tr><td width="100px">Extra Field 3</td><td>' + request.ps3 + '</td></tr>' : '') +
+      (request.ps4 ? '<tr><td width="100px">Extra Field 4</td><td>' + request.ps4 + '</td></tr>' : '') +
+      (request.ps5 ? '<tr><td width="100px">Extra Field 5</td><td>' + request.ps5 + '</td></tr>' : '') +
+      (request.ps6 ? '<tr><td width="100px">Extra Field 6</td><td>' + request.ps6 + '</td></tr>' : '') +
+      (request.ps7 ? '<tr><td width="100px">Extra Field 7</td><td>' + request.ps7 + '</td></tr>' : '') +
+      (request.ps8 ? '<tr><td width="100px">Extra Field 8</td><td>' + request.ps8 + '</td></tr>' : '') +
+      (request.eid ? '<tr><td width="100px">Element ID</td><td>' + request.eid + '</td></tr>' : '') +
+      (request.ecat ? '<tr><td width="100px">Element Category</td><td>' + request.ecat + '</td></tr>' : '') +
+      (request.lineNumber ? '<tr><td width="100px">Line Number</td><td>' + request.lineNumber + '</td></tr>' : '') +
+      (request.referralUrl ? '<tr><td width="100px">Referral Url</td><td>' + request.referralUrl + '</td></tr>' : '') +
+      (request.cd ? '<tr><td width="100px">Customer ID</td><td>' + request.cd + '</td></tr>' : '') +
+      (request.destinationUrl ? '<tr><td width="100px">Destination Url</td><td>' + request.destinationUrl + '</td></tr>' : '') +
+
+      '</tbody></table></div>'
+    )
+    filterTags();
+  });
